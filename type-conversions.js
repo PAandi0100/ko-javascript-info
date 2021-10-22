bfd585d5a1ac1e4a46a1ec9827d5dd63579c7e02
Pandi <pandi@wavetogether.com>
2021-10-22 09:53:40 +0900

    type-conversions

---
+++ type-conversions.js
@@ -0,0 +1,13 @@
+//7. operators
+let a = 1, b =1;
+let c = ++1; // c = 2
+let d = b++; // d = 1
+
+let a = 2;
+let x = 1 + (a *= 2); // x =5
+
+
+let a = +prompt("덧셈할 첫 번째 숫자를 입력해주세요.", 1);
+let b = +prompt("덧셈할 두 번째 숫자를 입력해주세요.", 2);
+
+alert(a + b);
