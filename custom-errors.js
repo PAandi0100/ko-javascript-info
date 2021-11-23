d249e4027addd8b5554d09503240ccbc6bde77e4
Pandi <pandi@wavetogether.com>
2021-11-23 19:03:13 +0900

    custom error + callback

--- custom-errors.js
+++ custom-errors.js
@@ -1,7 +1,14 @@
 ///38.
 
 //1.
+class SyntaxError{
+    constructor(message) {
+    }
+};
 
-
-
-
+class FormatError extends SyntaxError{
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+}
